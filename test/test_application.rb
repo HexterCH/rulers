7ceac1922dd879ce8d5744ad23b39f420cc8407c
Hexter Weng <hexter.ch@gmail.com>
2016-07-04 00:07:01 +0800

    update and fix test

--- test/test_application.rb
+++ test/test_application.rb
@@ -1,17 +1,26 @@
 require_relative "test_helper"
 
+class TestController < Rulers::Controller
+  def index
+    "Hello"
+  end
+end
+
 class TestApp < Rulers::Application
+  def get_controller_and_action(env)
+    [TestController, "index"]
+  end
 end
 
 class RulersAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
-  
+
   def app
     TestApp.new
   end
 
   def test_request
-    get "/"
+    get "/example/route"
 
     assert last_response.ok?
     body = last_response.body
