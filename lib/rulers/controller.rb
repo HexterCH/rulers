2955ee547d8605d630359acc1c635d4cdc9f1446
Hexter Weng <hexter.ch@gmail.com>
2016-07-03 23:01:44 +0800

    controller will detect where is the view's controller from

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,6 +1,8 @@
+require "erubis"
+
 module Rulers
   class Controller
-    def initialize(*args)
+    def initialize(env)
       @env = env
     end
 
@@ -8,11 +10,17 @@ module Rulers
       @env
     end
 
-    def render(view_name, locals={})
-      filename = File.join "app", "views", "#{view_name}.html.erb"
+    def controller_name
+      klass = self.class
+      klass = klass.to_s.gsub(/Controller$/, "")
+      Rulers.to_underscore klass
+    end
+
+    def render(view_name, locals = {})
+      filename = File.join "app", "views", controller_name, "#{view_name}.html.erb"
       template = File.read filename
       eruby = Erubis::Eruby.new(template)
-      eruby.result locals.merge(:env => env)
+      eruby.evaluate Erubis::Context.new locals.merge(:env => env)
     end
   end
 end
