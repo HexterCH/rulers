fbfc9c5f288670695141277ddec05e75061a0556
Hexter Weng <hexter.ch@gmail.com>
2016-07-15 01:36:45 +0800

    add route and get_rack_app method in route library

--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,11 +1,25 @@
+class RouteObject
+  def initialize
+    @rules = []
+  end
+
+  def match(url, *args)
+  end
+
+  def check_url
+  end
+end
+
 module Rulers
   class Application
-    def get_controller_and_action(env)
-      _, cont, action, after = env["PATH_INFO"].split('/', 4)
-      cont = cont.capitalize
-      cont += "Controller"
+    def route(&block)
+      @route_obj ||= RouteObject.new
+      @route_obj.instance_eval(&block)
+    end
 
-      [Object.const_get(cont), action]
+    def get_rack_app(env)
+      raise "No routes!" unless @route_obj
+      @route_obj.check_url env["PATH_INFO"]
     end
   end
 end
